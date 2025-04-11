a2d48c591aea84b85c00500a6a7b467a50272c17
GustavoKasaki <gmkasaki@gmail.com>
2025-04-11 16:42:06 -0300

    {chore} updated Hero + About sections

--- src/components/About.vue
+++ src/components/About.vue
@@ -5,12 +5,27 @@ export default {
 </script>
 
 <template>
-  <section id="about" class="bg-white px-6 py-40 transition-colors duration-500 dark:bg-gray-900">
-    <h3 class="mb-6 text-2xl font-semibold text-blue-600 dark:text-blue-400">About Me</h3>
-    <p class="mx-auto max-w-3xl leading-relaxed text-slate-600 dark:text-slate-300">
-      I'm a front-end developer with a background in mechanical engineering. Passionate about
-      building accessible, responsive, and user-centered applications. I love turning complex
-      problems into elegant and performant web solutions.
-    </p>
+  <section
+    id="about"
+    class="flex justify-center bg-white px-30 py-30 transition-colors duration-500 dark:bg-gray-900"
+  >
+    <img
+      src="@/assets/profile.jpg"
+      alt="Profile Picture"
+      class="mx-auto block h-70 rounded-full duration-500 ease-in-out dark:hidden"
+    />
+    <img
+      src="@/assets/profile-dark.jpg"
+      alt="Dark Profile Picture"
+      class="mx-auto hidden h-70 rounded-full duration-500 ease-in-out dark:block"
+    />
+    <div class="my-auto">
+      <h3 class="mb-6 text-2xl font-semibold text-blue-600 dark:text-blue-400">About Me</h3>
+      <p class="mx-auto max-w-3xl leading-relaxed text-slate-600 dark:text-slate-300">
+        I'm a front-end developer with a background in mechanical engineering. Passionate about
+        building accessible, responsive, and user-centered applications. I love turning complex
+        problems into elegant and performant web solutions.
+      </p>
+    </div>
   </section>
 </template>
--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -9,14 +9,14 @@ export default {
 
 <template>
   <section
-    class="bg-gray-100 px-6 py-40 text-center transition-colors duration-500 dark:bg-gray-800"
+    class="bg-gray-100 px-6 py-30 text-center transition-colors duration-500 dark:bg-gray-800"
   >
     <VueWriter
       :array="['Transforming ideas into modern web experiences.']"
       :iterations="1"
       :typeSpeed="80"
       :caret="cursor"
-      class="mb-4 text-4xl font-bold text-blue-600 transition-all duration-500 dark:text-blue-400"
+      class="mb-4 text-4xl font-bold text-blue-600 transition-all duration-500 sm:h-full dark:text-blue-400"
     />
     <p
       class="mx-auto mb-8 max-w-2xl text-lg text-slate-500 transition-all duration-500 dark:text-slate-300"
@@ -25,15 +25,14 @@ export default {
       functional and purposeful digital solutions.
     </p>
     <div class="space-x-4">
-      <!-- <button
-        class="cursor-pointer rounded bg-blue-600 px-6 py-2 text-white transition duration-500 ease-in-out hover:bg-blue-500"
-      >
-        <a href="#projects"> View Projects </a>
-      </button> -->
       <button
         class="cursor-pointer rounded border border-blue-600 px-6 py-2 text-blue-600 transition duration-500 ease-in-out hover:bg-blue-200 dark:border-blue-400 dark:text-blue-400 dark:hover:bg-gray-700"
       >
-        Download CV
+        <a
+          href="https://drive.google.com/file/d/1a7CPaih8AYOwYpJdOYRXEHuDTMJ0IVEq/view?usp=drive_link"
+        >
+          Download CV
+        </a>
       </button>
     </div>
   </section>
