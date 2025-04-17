2b57fd3086710c3f91758e0073c3414449c2d8c2
GustavoKasaki <gmkasaki@gmail.com>
2025-04-17 16:09:14 -0300

    {feat} added 'Pinia Plugin Persistedstate'  for store persistence

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import piniaPluginPersistedState from 'pinia-plugin-persistedstate'
 import App from './App.vue'
 import './assets/main.css'
 
 const app = createApp(App)
-app.use(createPinia())
+const pinia = createPinia()
+
+pinia.use(piniaPluginPersistedState)
+app.use(pinia)
 app.mount('#app')
